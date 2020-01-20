281e36a7863e844ffd4958784b3285c9380755d5
Sachida Giri <sachidag@chetu.com>
2020-01-20 15:02:13 +0530

    correct the routing error

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    Chidl component
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
---
+++ src/views/Contact.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Counter</h1>
+    <button @click="Incrememnt">Increment</button>
+    {{ counter }}
+    {{ doubCounter }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 10,
+      doubCounter: 0
+    };
+  },
+  methods: {
+    Incrememnt() {
+      this.counter++;
+      this.doubCounter = this.counter + 2;
+    }
+  }
+};
+</script>
